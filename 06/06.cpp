c542254a568344d8c27a8e4d9b34facc968fa3e1
lSoleyl <lSoleyl+github@gmail.com>
2024-12-07 10:35:22 +0100

    Day 6 Part 2: Optimized to run code in just 40-45ms  - Stored obstacleVector by-value in the state  - Now only storing pos+orientation of obstacles, which requires more steps to detect a loop, but halves execution speed as growing the set is the more expensive operation  - Finally completely disabled storing of positions for all but the first run using if constexpr.  - Fixed wrong Part2 result in comment

--- 06/06.cpp
+++ 06/06.cpp
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <string_view>
 #include <execution>
+#include <chrono>
 
 struct Vector {
   Vector(int column = 0, int row = 0) : column(column), row(row) {}
@@ -114,12 +115,14 @@ std::ostream& operator<<(std::ostream& out, const Field& field) {
   return out;
 }
 
+template<bool storeVisited>
 struct State {
-  State(Field& field, const Vector& position, const Vector& direction) : field(&field), position(position), direction(direction), obstacle(nullptr) {}
+  State(Field& field, const Vector& position, const Vector& direction) : field(&field), position(position), direction(direction), obstaclePos(-1,-1) {}
+  State(const State<!storeVisited>& other) : field(other.field), position(other.position), direction(other.direction), obstaclePos(other.obstaclePos),
+    visited(other.visited), visitedObstacles(other.visitedObstacles), inLoop(other.inLoop) {}
+
   enum class Result { LEFT_FIELD, IN_LOOP };
   enum class Action { STEP, ROTATE };
-
-
 
 
   Result run() {
@@ -132,12 +135,17 @@ struct State {
 
   
   void nextStep() {
-    visited.insert(position);
-    visitedOrientations.emplace(position, direction);
+    if constexpr (storeVisited) { 
+      // only needed for first run -> so just don't do it for the obstacle checks as this is actually quite expensive
+      visited.insert(position);
+    }
 
     auto nextPosition = position + direction;
-    if (field->validPosition(nextPosition) && ((*field)[nextPosition] == '#' || (obstacle && nextPosition == *obstacle))) {
+    if (field->validPosition(nextPosition) && ((*field)[nextPosition] == '#' || nextPosition == obstaclePos)) {
       // obstacle in front -> rotate
+      if (!visitedObstacles.emplace(position, direction).second) {
+        inLoop = true;
+      }
       direction = direction.rotateCW();
     } else {
       // continue moving in that direction
@@ -146,17 +154,16 @@ struct State {
   }
 
   bool leftField() const { return !field->validPosition(position); }
-  bool stuckInLoop() const { return visitedOrientations.contains(std::make_pair(position, direction)); }
+  bool stuckInLoop() const { return inLoop; }
 
   Field* field;
   std::unordered_set<Vector> visited;
-  std::unordered_set<std::pair<Vector/*pos*/, Vector/*direction*/>> visitedOrientations;
+  std::unordered_set<std::pair<Vector/*pos*/, Vector/*direction*/>> visitedObstacles;
   Vector position;
   Vector direction;
-  Vector* obstacle; // additional obstacle (used to check multiple potential obstacles in parallel without modifying the field)
+  Vector obstaclePos; // additional obstacle (used to check multiple potential obstacles in parallel without modifying the field)
+  bool inLoop = false;
 };
-
-#include <chrono>
 
 int main() {
   auto t1 = std::chrono::high_resolution_clock::now();
@@ -166,8 +173,8 @@ int main() {
   auto startPosition = field.fromOffset(field.data.find('^'));
 
   
-  const State startState(field, startPosition, Vector(0, -1)); // start direction is UP as rows increment downward
-  State firstRun(startState);
+  const State<false> startState(field, startPosition, Vector(0, -1)); // start direction is UP as rows increment downward
+  State<true> firstRun(startState);
   firstRun.run();
   
   // Part2 : At each visited position [except for the start position] try adding an obstacle and check whether this causes a loop
@@ -179,19 +186,23 @@ int main() {
 
 
   // Parellelized solution as original takes 5.2 seconds, parallel one takes 1.1 seconds (or 780ms without debugger)
+  // Reduced further down to 2,5s or 0,5s (parallel) by only storing the obstacle orientations for loop detection
+  // Further optimized down to 195ms or 45ms (parallel) by not storing visited states at all when performing obstacle loop checks
   std::for_each(std::execution::par, firstRun.visited.begin(), firstRun.visited.end(), [&](Vector position) {
-    State state(startState);
-    state.obstacle = &position;
-    if (state.run() == State::Result::IN_LOOP) {
-      std::lock_guard<std::mutex> guard(m); // synchronize access to set!
-      possibleLoops.insert(position);
+    if (position != startPosition) {
+      State<false> state(startState);
+      state.obstaclePos = position;
+      if (state.run() == State<false>::Result::IN_LOOP) {
+        std::lock_guard<std::mutex> guard(m); // synchronize access to set!
+        possibleLoops.insert(position);
+      }
     }
   });
 
   auto t2 = std::chrono::high_resolution_clock::now();
 
   std::cout << "Part 1: " << firstRun.visited.size() << "\n"; // 4752
-  std::cout << "Part 2: " << possibleLoops.size() << "\n"; // 1838
+  std::cout << "Part 2: " << possibleLoops.size() << "\n"; // 1719
 
   std::cout << "Time " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms\n";
 }
