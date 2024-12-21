bfa1a66748c13ba7f260b0976fdfcf0424090ec7
lSoleyl <lSoleyl+github@gmail.com>
2024-12-21 13:37:03 +0100

    Day 21 Part 2 completed  - Was actually easy with the setup I had for the first part  - I had to remove the stringified path versions as memory was going well past 8GB while constructing the lookup maps

--- 21/21.cpp
+++ 21/21.cpp
@@ -28,7 +28,7 @@ Keypad controls = {
 
 // We can statically precalculate the cost of pressing a button in a later number pad, because
 // we can use the fact that the earlier pads will always confirm each press with an 'A' and thus start and end at an 'A'
-using CostMap = std::unordered_map<std::pair<Vector/*from*/, Vector/*to*/>, std::pair<int/*buttonPresses*/, std::string/*buttonSequence*/>>;
+using CostMap = std::unordered_map<std::pair<Vector/*from*/, Vector/*to*/>, int64_t/*buttonpresses*/>;
 
 
 struct KeypadState {
@@ -38,7 +38,7 @@ struct KeypadState {
       if (fromButton != ' ') {
         for (auto& [toButton, toPos] : keypad) {
           if (toButton != ' ') {
-            costMap.insert({ {fromPos, toPos}, {1, std::string(1, toButton) }});
+            costMap.insert({ {fromPos, toPos}, 1});
           }
         }
       }
@@ -67,13 +67,13 @@ struct KeypadState {
   }
 
 
-  std::pair<int, std::string> getLowestPathCost(Vector from, Vector to, Vector posLowerLevel) const {
+  int64_t getLowestPathCost(Vector from, Vector to, Vector posLowerLevel) const {
     if (from == to) {
       // The costs are the costs to press the 'A' button on the lower level
       return prevKeypad->costMap.at({ posLowerLevel, prevKeypad->keypad.at('A') });
     }
 
-    std::pair<int, std::string> minCosts = { std::numeric_limits<int>::max(), "???" };
+    auto minCosts = std::numeric_limits<int64_t>::max();
 
     // Try to go in each direction, which reduces the step distance to the target
     //for (auto direction : Vector::AllDirections()) {
@@ -84,8 +84,8 @@ struct KeypadState {
         auto nextLowerLevelPos = prevKeypad->keypad.at(direction.toChar()); // where we move on the next lower level
         auto stepEntry = prevKeypad->costMap.at({posLowerLevel, nextLowerLevelPos }); // get cost move performing this move on the lower level
         auto costEntry = getLowestPathCost(nextPos, to, nextLowerLevelPos);
-        if (stepEntry.first + costEntry.first < minCosts.first) {
-          minCosts = { stepEntry.first + costEntry.first, stepEntry.second + costEntry.second };
+        if (stepEntry + costEntry < minCosts) {
+          minCosts = stepEntry + costEntry;
         }
       }
     }
@@ -95,24 +95,16 @@ struct KeypadState {
 
 
 
-  int getSequenceCost(const std::string& sequence) const {
+  int64_t getSequenceCost(const std::string& sequence) const {
 
-    int buttonPresses = 0;
-
-    std::cout << sequence << " : ";
+    int64_t buttonPresses = 0;
 
     auto position = keypad.at('A'); // we start input of each sequence with the robot arm at 'A'
     for (auto button : sequence) {
       auto buttonPos = keypad.at(button);
-
-      auto costEntry = costMap.at({ position, buttonPos });
-      std::cout << costEntry.second << ' ';
-
-      buttonPresses += costEntry.first;
+      buttonPresses += costMap.at({ position, buttonPos });
       position = buttonPos; // update position
     }
-
-    std::cout << "\n";
 
     // Calculate sequence cost as the numeric value times the number of button presses required
     return std::stoi(sequence) * buttonPresses;
@@ -130,7 +122,7 @@ struct KeypadState {
 struct RobotControl {
   RobotControl(std::istream&& input) : sequences(std::istream_iterator<std::string>(input), std::istream_iterator<std::string>()) {}
 
-  int countSequenceComplexities() const {
+  auto countSequenceComplexities() const {
     
     // List of control pads in reverse order
     std::list<KeypadState> controlPads = { controls }; // user controlled
@@ -139,7 +131,28 @@ struct RobotControl {
     
     KeypadState numberPad = { numbers, controlPads.front() }; // robot controlled (don't calculate the costs here)
 
-    int totalComplexity = 0;
+    int64_t totalComplexity = 0;
+
+    // Now go through each sequence
+    for (auto& sequence : sequences) {
+      totalComplexity += numberPad.getSequenceCost(sequence);
+    }
+
+    return totalComplexity;
+  }
+
+  auto countSequenceComplexities2() const {
+
+    // List of control pads in reverse order
+    std::list<KeypadState> controlPads = { controls }; // user controlled
+
+    for (int i = 0; i < 25; ++i) {
+      controlPads.push_front({ controls, controlPads.front() }); // robot controlled
+    }
+    
+    KeypadState numberPad = { numbers, controlPads.front() }; // robot controlled (don't calculate the costs here)
+
+    int64_t totalComplexity = 0;
 
     // Now go through each sequence
     for (auto& sequence : sequences) {
@@ -158,8 +171,10 @@ int main() {
 
   RobotControl control(std::ifstream("input.txt"));
   auto complexities = control.countSequenceComplexities();
+  auto complexities2 = control.countSequenceComplexities2();
 
   auto t2 = std::chrono::high_resolution_clock::now();
   std::cout << "Part 1: " << complexities << "\n"; // 270084
+  std::cout << "Part 2: " << complexities2 << "\n"; // 329431019997766
   std::cout << "Time " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms\n";
 }
